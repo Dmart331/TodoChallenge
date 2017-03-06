bcd543c1ee5fd371bcf8e6c56c9ac7592b4949f6
Paul Martin <Dmart331@gmail.com>
2017-03-06 13:57:23 -0600

    fixed crashing bug

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -3,22 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { CompletedFilterPipe} from './index';
 
 
 
 @NgModule({
   declarations: [
-    CompletedFilterPipe,
     AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
-  ],
-  exports:[
-    CompletedFilterPipe
   ],
   providers: [],
   bootstrap: [AppComponent]
